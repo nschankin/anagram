2f16147fd59080126b27a42fc5bfc792fbae9cd2
nikolaischankin <nikolai.schankin@gmx.de>
2024-02-19 10:38:09 +0100

    feat: create cli access print matching mode

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # anagram
+
+Preconditions:
+JDK 21 is required for compiling
+JRE 21 is required fro running this program.
--- src/main/java/de/nschankin/anagram/AnagramApp.java
+++ src/main/java/de/nschankin/anagram/AnagramApp.java
@@ -1,15 +1,41 @@
 package de.nschankin.anagram;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnagramApp {
+
+    private static final String CHECK_MODE = "check";
+
+    private static final String PRINT_MATCHING_MODE = "printMatching";
 
     public static void main(String [] args){
         if(args.length == 0){
             printUsage();
-        } else if("check".equals(args[0]) && args.length == 3){
+        } else if(CHECK_MODE.equals(args[0]) && args.length == 3){
             AnagramChecker checker = new AnagramChecker(args[1],args[2]);
-            System.out.println("checking '" + args[1] + "' and '" + args[2] +"'");
+            System.out.println(CHECK_MODE + "ing '" + args[1] + "' and '" + args[2] +"'");
 
             System.out.println("Result: " +  checker.isAnagram());
+        } else if (PRINT_MATCHING_MODE.equals(args[0]) && args.length > 2) {
+
+            List<String> stringList = new ArrayList<>(List.of(args));
+            //remove command
+            stringList.remove(0);
+            String source = stringList.remove(0);
+            System.out.println("Source phrase");
+            System.out.println(source);
+
+            stringList.forEach(currentString -> {
+                AnagramChecker checker = new AnagramChecker(source,currentString);
+                if (checker.isAnagram()){
+                    System.out.println("Matching anagram:");
+                    System.out.println(currentString);
+                }
+            });
+
+        } else {
+            printUsage();
         }
 
     }
@@ -18,13 +44,23 @@ public class AnagramApp {
     private static void printUsage(){
         System.out.println("The anagram app has two modes:");
         System.out.println("-----------------------------------------------------------------------");
-        System.out.println("1.) check");
-        System.out.println("check if to phrases are anagrams and print 'true' or 'false'");
+        System.out.println("1.) " + CHECK_MODE);
+        System.out.println("Checks if to phrases are anagrams and print 'true' or 'false'");
         System.out.println("Example:");
-        System.out.println("check 'Silent!' 'Listen!'");
+        System.out.println(CHECK_MODE + " 'Silent!' 'Listen!'");
         System.out.println("will print 'true'");
-
-
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("2.) " + PRINT_MATCHING_MODE);
+        System.out.println("prints all phrases which match the first argument (source)");
+        System.out.println("Example:");
+        System.out.println(PRINT_MATCHING_MODE + " printMatching 'Silent, yes' 'Listen !!!! yes' 'sey tenlis' 'No match'");
+        System.out.println("will print:");
+        System.out.print("Source phrase\n" +
+                "Silent, yes\n" +
+                "Matching Anagram:\n" +
+                "Listen !!!! yes\n" +
+                "Matching Anagram:\n" +
+                "sey tenlis\n");
         System.out.println("-----------------------------------------------------------------------");
     }
 }
